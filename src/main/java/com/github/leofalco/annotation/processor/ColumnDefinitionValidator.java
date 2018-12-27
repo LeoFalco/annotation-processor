22895c80b19a3c5ce1ef3fb87781077731fc5766
Leonardo Jesus Falco <leonardo.falco@outlook.com>
2018-12-27 17:58:33 -0200

    .

---
+++ src/main/java/com/github/leofalco/annotation/processor/ColumnDefinitionValidator.java
@@ -0,0 +1,42 @@
+package com.github.leofalco.annotation.processor;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.persistence.Enumerated;
+import java.util.List;
+import java.util.Set;
+
+@SupportedAnnotationTypes("javax.persistence.Enumerated")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
+public class ColumnDefinitionValidator extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+
+        for (Element element : roundEnv.getElementsAnnotatedWith(Enumerated.class)) {
+            List<? extends AnnotationMirror> annotationMirrors = element.getAnnotationMirrors();
+
+            element.getAnnotationsByType()
+
+            for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
+                DeclaredType annotationType = annotationMirror.getAnnotationType();
+
+
+
+
+            }
+
+
+        }
+
+
+        return false;
+    }
+}
